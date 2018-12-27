1387d6be84a6ce563479cfaaa55bcc56121f1835
cole smith <colesmith@Coles-iMac.local>
2018-12-26 23:13:41 -0700

    started working on styles and reworked button componet and made socail icon buttons

--- one-commercial/src/components/listingpage/photoArray.js
+++ one-commercial/src/components/listingpage/photoArray.js
@@ -1,18 +1,58 @@
+// https://www.npmjs.com/package/lightbox-react
 import React from 'react';
+import Lightbox from 'lightbox-react';
+import 'lightbox-react/style.css';
 import style from "../../components/styles/listingPage/photoArray.module.sass"
+import { array } from 'prop-types';
+
 class photoArray extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            photoIndex: 0,
+            isOpen: false,
+            images: []
+        }
+        this.buildarray = this.buildarray.bind(this);
+    }
+    buildarray() {
+        const tempArray = []
+        this.props.imageArray.forEach(element => {
+            tempArray.push(element.file.url)
+        });
+        tempArray.push(this.props.altImage.file.url)
+        this.setState({ images: tempArray })
+
+    }
+    componentDidMount() {
+        this.buildarray()
     }
 
-
     render() {
-        console.log(this.props);
+
+        const { photoIndex, isOpen } = this.state;
         return (
             this.props.imageArray.map((img, index) => (
                 <div>
-                    <img src={img.file.url} alt="" />
+                    <img key={index} src={img.file.url} alt="" onClick={() => this.setState({ isOpen: true })} />
+                    {isOpen && (
+                        <Lightbox
+                            mainSrc={this.state.images[photoIndex]}
+                            nextSrc={this.state.images[(photoIndex + 1) % this.state.images.length]}
+                            prevSrc={this.state.images[(photoIndex + this.state.images.length - 1) % this.state.images.length]}
+                            onCloseRequest={() => this.setState({ isOpen: false })}
+                            onMovePrevRequest={() =>
+                                this.setState({
+                                    photoIndex: (photoIndex + this.state.images.length - 1) % this.props.imageArray.length,
+                                })
+                            }
+                            onMoveNextRequest={() =>
+                                this.setState({
+                                    photoIndex: (photoIndex + 1) % this.state.images.length,
+                                })
+                            }
+                        />
+                    )}
                 </div>
             ))
         );
