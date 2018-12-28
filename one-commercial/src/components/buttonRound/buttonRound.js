f84d9bdd6e27116ad2ffdba6fb76d195cf7e6ccb
cole smith <colesmith@Coles-iMac.local>
2018-12-28 00:10:00 -0700

    fixed button working now

--- one-commercial/src/components/buttonRound/buttonRound.js
+++ one-commercial/src/components/buttonRound/buttonRound.js
@@ -2,10 +2,7 @@
 // innerText "what the button should say"
 // type "link" or "function"
 // action "url" or function
-
-import { Link } from 'gatsby'
 import React from 'react'
-
 import { Link } from 'gatsby'
 //styles
 import style from './buttonround.module.sass'
@@ -29,32 +26,17 @@ class ButtonRound extends React.Component {
 
         } else if (this.props.type === "function") {
             button = <button className={style.buttonRound} onClick={this.props.action} style={{ color: this.props.color, borderColor: this.props.color, padding: this.props.padding, fontSize: `${this.props.fsize}rem` }}>{this.props.innerText}</button>
-        }else if (this.props.type === 'gatsbylink') {
-              button = (
-                <Link
-                  className={style.buttonRound}
-                  to={/${this.props.action}}
-                >
-                  <button style={color: this.props.color, borderColor: this.props.color, padding: this.props.padding, fontSize: `${this.props.fsize}rem` }}> {this.props.innerText}</button>
-                </Link>
-              )
-            }
+        } else if (this.props.type === 'gatsbylink') {
+            button = <Link to={`/${this.props.action}`}><button className={style.buttonRound} style={{ color: this.props.color, borderColor: this.props.color, padding: this.props.padding, fontSize: `${this.props.fsize}rem` }}> {this.props.innerText} </button> </Link >
+
+        }
+
         return (
             <div className={style.wraper} style={{ justifyContent: this.props.pos }}>
                 {button}
             </div >
-
-
-        );
-
-
+        )
     }
-    return (
-      <div className={style.wraper} style={{ justifyContent: this.props.pos }}>
-        {button}
-      </div>
-    )
-  }
 }
 
 export default ButtonRound
