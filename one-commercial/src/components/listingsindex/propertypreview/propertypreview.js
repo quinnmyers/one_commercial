a189423aa23229e07c355c64eb6ba5f1b4f7a9f7
Quinn <quinnsmyers@gmail.com>
2018-12-29 15:05:49 -0800

    styled listing indexes for desktop

--- one-commercial/src/components/listingsindex/propertypreview/propertypreview.js
+++ one-commercial/src/components/listingsindex/propertypreview/propertypreview.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby-link'
+import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
 
 //styles
 import './propertypreview.sass'
+import Content from '../../content'
 
 const PropertyPreview = ({
   name,
@@ -16,17 +18,56 @@ const PropertyPreview = ({
   underContractPending,
   desc,
 }) => (
-  <ul>
-    <li>{name}</li>
-    <li>{address}</li>
-    {/* <li>{image}</li> */}
-    <li>{salePrice}</li>
-    <li>{category}</li>
-    <li>{buildinglotSize}</li>
-    <li>{measurementUnit}</li>
-    <li>{underContractPending ? 'true' : 'false'}</li>
-    <li>{desc}</li>
-  </ul>
+  <Content>
+    <div className="property__preview">
+      <div className="property__preview__container">
+        <div className="property__preview__container__image">
+          <Img fluid={image} />
+        </div>
+        <div className="property__preview__container__info">
+          <div className="property__preview__container__info__left">
+            <h3>{name}</h3>
+            <div dangerouslySetInnerHTML={{ __html: address }} />
+            <div className="property__preview__container__info__left__price">
+              $ {salePrice.toLocaleString()}
+            </div>
+          </div>
+          <div className="property__preview__container__info__right">
+            <h5>Category</h5>
+            <p>{category}</p>
+            <h5>Bldg/Lot Size</h5>
+            <p>
+              {buildinglotSize.toLocaleString()} {measurementUnit}
+            </p>
+            <h5>In Contract/Pending?</h5>
+            <p>{underContractPending ? 'Yes' : 'No'}</p>
+          </div>
+        </div>
+        <div className="property__preview__container__description">
+          <div dangerouslySetInnerHTML={{ __html: desc.substring(0, 400) }} />
+        </div>
+      </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <ul>
+      <li>{name}</li> --
+      <li>{address}</li> --
+      {/* <li>{image}</li> */}
+      <li>{salePrice}</li> --
+      <li>{category}</li> --
+      <li>{buildinglotSize}</li>--
+      <li>{measurementUnit}</li>--
+      <li>{underContractPending ? 'true' : 'false'}</li>--
+      <li>{desc}</li>
+    </ul>
+  </Content>
 )
 
 PropertyPreview.propTypes = {
