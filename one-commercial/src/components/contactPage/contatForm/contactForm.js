15cebae7a0c6c032ef3777c1b24c94c58e25697a
cole smith <colesmith@Coles-iMac.local>
2018-12-30 16:03:53 -0700

    working on mobile styles and got the rest done

--- one-commercial/src/components/contactPage/contatForm/contactForm.js
+++ one-commercial/src/components/contactPage/contatForm/contactForm.js
@@ -5,6 +5,20 @@ class ContactForm extends React.Component {
         super(props);
         this.state = {}
         this.selectRef = React.createRef()
+        this.focusHandler = this.focusHandler.bind(this)
+        this.blurHandler = this.blurHandler.bind(this)
+    }
+    focusHandler(e) {
+        e.persist()
+        const oldPlaceholder = e.target.placeholder
+        e.target.placeholder = ""
+        this.setState({ oldPlaceholder: oldPlaceholder })
+    }
+    blurHandler(e) {
+        e.persist()
+        if (e.target.value === "") {
+            e.target.placeholder = this.state.oldPlaceholder
+        }
     }
     componentDidMount() {
         console.log(this.selectRef.current);
@@ -17,29 +31,41 @@ class ContactForm extends React.Component {
     render() {
         return (
             <form action="https://formspree.io/your@email.com" method="POST" className={style.wraper}>
-                <label htmlFor="listings">Which property are you interested in?</label>
-                <select ref={this.selectRef} id="listings">
-                    <option disabled value>Please select one</option>
-                    {this.props.listings.map((link, index) => (
+                <div className={style.inputblock}>
+                    <label htmlFor="listings">Which property are you interested in?</label>
+                    <select ref={this.selectRef} id="listings">
+                        <option disabled value>Please select one</option>
+                        {this.props.listings.map((link, index) => (
 
-                        <option
-                            value={link.name} key={index}>
-                            {link.name}
+                            <option
+                                value={link.name} key={index}>
+                                {link.name}
 
-                        </option>
-                    ))}
+                            </option>
+                        ))}
 
-                </select>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" placeholder="Your Name..." />
-                <label htmlFor="phone">Phone</label>
-                <input type="text" id="phone" placeholder="Your Phone Number..." />
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="_replyto" placeholder="Your Email..." />
-                <label htmlFor="message">Message</label>
-                <textarea name="" id="message" cols="30" rows="10" placeholder="Enter Your Message..."></textarea>
-                <input type="submit" value="Send Message" />
-            </form>
+                    </select>
+                </div>
+                <div className={style.inputblock}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Your Name..." onFocus={this.focusHandler} onBlur={this.blurHandler} />
+                </div>
+                <div className={style.inputblock}>
+                    <label htmlFor="phone">Phone</label>
+                    <input type="text" id="phone" placeholder="Your Phone Number..." onFocus={this.focusHandler} onBlur={this.blurHandler} />
+                </div>
+                <div className={style.inputblock}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="_replyto" placeholder="Your Email..." onFocus={this.focusHandler} onBlur={this.blurHandler} />
+                </div>
+                <div className={style.inputblock}>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="" id="message" cols="30" rows="6" placeholder="Enter Your Message..." onFocus={this.focusHandler} onBlur={this.blurHandler}></textarea>
+                </div>
+                <div className={style.inputsubmit}>
+                    <input type="submit" value="Send Message" />
+                </div>
+            </form >
         );
     }
 }
--- one-commercial/src/components/contactPage/hero/hero.js
+++ one-commercial/src/components/contactPage/hero/hero.js
@@ -6,6 +6,7 @@ import Content from '../../content'
 import ButtonRound from '../../buttonRound/buttonRound'
 
 //styles
+import style from "./hero.module.sass"
 import '../../listingsindex/listingindexhero/listingindexhero.sass'
 
 const Hero = ({
@@ -21,10 +22,10 @@ const Hero = ({
             style={{ backgroundImage: `url("${bgImage}")` }}
         >
             <Content>
-                <div className="listing__index__hero__content">
+                <div className="listing__index__hero__content" className={style.content}>
                     <div className="listing__index__hero__content__top">
                         <h2>{header}</h2>
-                        <p>{content}</p>
+                        <div className={style.content__tagline} dangerouslySetInnerHTML={{ __html: content }}></div>
                         <ButtonRound
                             action={buttonLink}
                             type="gatsbylink"
@@ -35,7 +36,7 @@ const Hero = ({
                     </div>
                 </div>
             </Content>
-        </div>
+        </div >
     )
 
 //add is required here later
