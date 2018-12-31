a96a874c01c5d269cb7af1d82018b77827c51384
cole smith <colesmith@Coles-iMac.local>
2018-12-31 01:01:23 -0700

    made sevices page

--- one-commercial/src/components/listingsindex/listingindexhero/listingindexhero.js
+++ one-commercial/src/components/listingsindex/listingindexhero/listingindexhero.js
@@ -16,31 +16,36 @@ const ListingIndexHero = ({
   subHeader,
   buttonText,
   buttonLink,
+  color = "white",
+  innerhtml = false
 }) => (
-  <div
-    className="listing__index__hero"
-    style={{ backgroundImage: `url("${bgImage}")` }}
-  >
-    <Content>
-      <div className="listing__index__hero__content">
-        <div className="listing__index__hero__content__top">
-          <h2>{header}</h2>
-          <p>{content}</p>
+    <div
+      className="listing__index__hero"
+      style={{ backgroundImage: `url("${bgImage}")`, color: color }}
+    >
+      <Content>
+        <div className="listing__index__hero__content">
+          <div className="listing__index__hero__content__top">
+            <h2>{header}</h2>
+            {(innerhtml === false) ?
+              <p>{content}</p> :
+              <div dangerouslySetInnerHTML={{ __html: content }}></div>
+            }
+          </div>
+          <div className="listing__index__hero__content__bottom">
+            <h3>{subHeader}</h3>
+            <ButtonRound
+              action={buttonLink}
+              type="gatsbylink"
+              innerText={buttonText}
+              color={color}
+              pos="left"
+            />
+          </div>
         </div>
-        <div className="listing__index__hero__content__bottom">
-          <h3>{subHeader}</h3>
-          <ButtonRound
-            action={buttonLink}
-            type="gatsbylink"
-            innerText={buttonText}
-            color="white"
-            pos="left"
-          />
-        </div>
-      </div>
-    </Content>
-  </div>
-)
+      </Content>
+    </div>
+  )
 
 //add is required here later
 ListingIndexHero.propTypes = {
