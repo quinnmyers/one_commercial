15ae9ee1f98f30429e22e99d2579f9ce76cd779e
Quinn <quinnsmyers@gmail.com>
2018-12-30 23:24:02 -0800

    added more rough desktop styling for featured listing section

--- one-commercial/src/components/home/featuredListing/featuredListing.js
+++ one-commercial/src/components/home/featuredListing/featuredListing.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 
 //components
 import ButtonRound from '../../buttonRound/buttonRound'
+import ShareLinkButtons from '../../listingpage/shareLinkButtons/shareLinkButtons'
 
 //styles
 import './featuredListing.sass'
@@ -14,31 +15,50 @@ const FeaturedListing = ({ name, address, size, unit, desc, image }) => {
     <div className="featured__listing">
       <div className="featured__listing__container">
         <div className="featured__listing__container__info">
-          <h2>Featured Listing</h2>
-          <div className="featured__listint__container__info__nameaddress">
+          <div className="featured__listing__container__info__nameaddress">
+            <h2>Featured Listing</h2>
             <h4>{name}</h4>
             <div dangerouslySetInnerHTML={{ __html: address }} />
           </div>
-          <div className="featured__listint__container__info__info">
-            {size}
-            {unit}
-          </div>
-          <div className="featured__listint__container__info__desc">
-            <div dangerouslySetInnerHTML={{ __html: desc.substring(0, 300) }} />
-          </div>
-          <div className="featured__listint__container__info__links">
+          <div className="featured__listing__container__info__desc">
+            <div dangerouslySetInnerHTML={{ __html: desc.substring(0, 400) }} />
             <ButtonRound
               action="#"
               type="gatsbylink"
-              innerText="Learn More About THe CHEAKAJDSA"
+              innerText={`Learn More About ${name}`}
               color="black"
               pos="left"
+            />
+            <ShareLinkButtons
+              icons={[
+                {
+                  icon:
+                    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTkgMGgtMTRjLTIuNzYxIDAtNSAyLjIzOS01IDV2MTRjMCAyLjc2MSAyLjIzOSA1IDUgNWgxNGMyLjc2MiAwIDUtMi4yMzkgNS01di0xNGMwLTIuNzYxLTIuMjM4LTUtNS01em0tMyA3aC0xLjkyNGMtLjYxNSAwLTEuMDc2LjI1Mi0xLjA3Ni44ODl2MS4xMTFoM2wtLjIzOCAzaC0yLjc2MnY4aC0zdi04aC0ydi0zaDJ2LTEuOTIzYzAtMi4wMjIgMS4wNjQtMy4wNzcgMy40NjEtMy4wNzdoMi41Mzl2M3oiLz48L3N2Zz4=',
+                  site: 'facebook',
+                },
+                {
+                  icon:
+                    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTkgMGgtMTRjLTIuNzYxIDAtNSAyLjIzOS01IDV2MTRjMCAyLjc2MSAyLjIzOSA1IDUgNWgxNGMyLjc2MiAwIDUtMi4yMzkgNS01di0xNGMwLTIuNzYxLTIuMjM4LTUtNS01em0tLjEzOSA5LjIzN2MuMjA5IDQuNjE3LTMuMjM0IDkuNzY1LTkuMzMgOS43NjUtMS44NTQgMC0zLjU3OS0uNTQzLTUuMDMyLTEuNDc1IDEuNzQyLjIwNSAzLjQ4LS4yNzggNC44Ni0xLjM1OS0xLjQzNy0uMDI3LTIuNjQ5LS45NzYtMy4wNjYtMi4yOC41MTUuMDk4IDEuMDIxLjA2OSAxLjQ4Mi0uMDU2LTEuNTc5LS4zMTctMi42NjgtMS43MzktMi42MzMtMy4yNi40NDIuMjQ2Ljk0OS4zOTQgMS40ODYuNDExLTEuNDYxLS45NzctMS44NzUtMi45MDctMS4wMTYtNC4zODMgMS42MTkgMS45ODYgNC4wMzggMy4yOTMgNi43NjYgMy40My0uNDc5LTIuMDUzIDEuMDgtNC4wMyAzLjE5OS00LjAzLjk0MyAwIDEuNzk3LjM5OCAyLjM5NSAxLjAzNy43NDgtLjE0NyAxLjQ1MS0uNDIgMi4wODYtLjc5Ni0uMjQ2Ljc2Ny0uNzY2IDEuNDEtMS40NDMgMS44MTYuNjY0LS4wOCAxLjI5Ny0uMjU2IDEuODg1LS41MTctLjQzOS42NTYtLjk5NiAxLjIzNC0xLjYzOSAxLjY5N3oiLz48L3N2Zz4=',
+                  site: 'twitter',
+                },
+                {
+                  icon:
+                    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTkgMGgtMTRjLTIuNzYxIDAtNSAyLjIzOS01IDV2MTRjMCAyLjc2MSAyLjIzOSA1IDUgNWgxNGMyLjc2MiAwIDUtMi4yMzkgNS01di0xNGMwLTIuNzYxLTIuMjM4LTUtNS01em0tMTEgMTloLTN2LTExaDN2MTF6bS0xLjUtMTIuMjY4Yy0uOTY2IDAtMS43NS0uNzktMS43NS0xLjc2NHMuNzg0LTEuNzY0IDEuNzUtMS43NjQgMS43NS43OSAxLjc1IDEuNzY0LS43ODMgMS43NjQtMS43NSAxLjc2NHptMTMuNSAxMi4yNjhoLTN2LTUuNjA0YzAtMy4zNjgtNC0zLjExMy00IDB2NS42MDRoLTN2LTExaDN2MS43NjVjMS4zOTYtMi41ODYgNy0yLjc3NyA3IDIuNDc2djYuNzU5eiIvPjwvc3ZnPg==',
+                  site: 'linkedin',
+                },
+              ]}
             />
           </div>
         </div>
       </div>
       <div className="featured__listing__spacer" />
-      <div className="featured__listing__image" />
+      <div className="featured__listing__image">
+        <Img
+          fluid={image}
+          className="featured-listing-image"
+          alt={`${name} located at ${address}`}
+        />
+      </div>
     </div>
   )
 }
