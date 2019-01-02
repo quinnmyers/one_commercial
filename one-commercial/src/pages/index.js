c1a3b1c501701dbb351d4e7d206c408f6ffd258e
Quinn <quinnsmyers@gmail.com>
2019-01-01 22:11:03 -0800

    fixed small issue with z-index in header and lightbox

--- one-commercial/src/pages/index.js
+++ one-commercial/src/pages/index.js
@@ -27,8 +27,8 @@ class IndexPage extends Component {
     super(props)
     this.state = {}
   }
-  componentWillMount() { }
-  componentDidMount() { }
+  componentWillMount() {}
+  componentDidMount() {}
   render() {
     const { data } = this.props
     const indexAssets = data.contentfulHomePage
@@ -53,7 +53,7 @@ class IndexPage extends Component {
           content={indexAssets.heroParagraph.internal.content}
           subHeader={indexAssets.heroSubheader}
           buttonText={indexAssets.heroButtonText}
-          buttonLink="saleindex"
+          buttonLink="services"
         />
         {/* <ServicesCta
           underHeroHeader={indexAssets.underHeroTextBlockBold}
@@ -67,9 +67,9 @@ class IndexPage extends Component {
         <FeaturedListing
           pageUrl={`${
             this.props.location.href
-            }${this.props.data.contentfulPropertiesOnWebsite.featuredProperty.name
-              .split(' ')
-              .join('-')}`}
+          }${this.props.data.contentfulPropertiesOnWebsite.featuredProperty.name
+            .split(' ')
+            .join('-')}`}
           name={featuredQuery.name}
           address={featuredQuery.address.childContentfulRichText.html}
           size={featuredQuery.buildinglotSize}
