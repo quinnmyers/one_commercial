2ceab5d89364a396b633acb21eb0fd2f18a09ceb
Quinn <quinnsmyers@gmail.com>
2019-01-06 23:10:35 -0800

    fixed issue with footer links and font sizing on contact page

--- one-commercial/src/components/layout/footer/footer-right.js
+++ one-commercial/src/components/layout/footer/footer-right.js
@@ -14,15 +14,18 @@ const FooterRight = () => (
           </Link>
         </li>
         <li className="footer__right__content__list__item">
-          <Link to="/listings/">
+          <Link to="/saleindex/">
             <h6 className="footer__right__content__list__item__main">
               Listings
             </h6>
           </Link>
-
-          <p className="footer__right__content__list__item__sub">For Lease</p>
-          <p className="footer__right__content__list__item__sub">For Sale</p>
-          <p className="footer__right__content__list__item__sub">For Sold</p>
+          <Link to="/leaseindex/">
+            <p className="footer__right__content__list__item__sub">For Lease</p>
+          </Link>
+          <Link to="/saleindex/">
+            <p className="footer__right__content__list__item__sub">For Sale</p>
+          </Link>
+          {/* <Link> <p className="footer__right__content__list__item__sub">For Sold</p></Link> */}
         </li>
         <li className="footer__right__content__list__item">
           <Link to="/services/">
@@ -30,19 +33,26 @@ const FooterRight = () => (
               Services
             </h6>
           </Link>
-
-          <p className="footer__right__content__list__item__sub">
-            Listing Services
-          </p>
-          <p className="footer__right__content__list__item__sub">
-            Property Management
-          </p>
-          <p className="footer__right__content__list__item__sub">
-            Development Services
-          </p>
-          <p className="footer__right__content__list__item__sub">
-            Investment Strategy
-          </p>
+          <Link to="/services/" state={{ toID: 'Listing_Services' }}>
+            <p className="footer__right__content__list__item__sub">
+              Listing Services
+            </p>
+          </Link>
+          <Link to="/services/" state={{ toID: 'Property_Management' }}>
+            <p className="footer__right__content__list__item__sub">
+              Property Management
+            </p>
+          </Link>
+          <Link to="/services/" state={{ toID: 'Development_Services' }}>
+            <p className="footer__right__content__list__item__sub">
+              Development Services
+            </p>
+          </Link>
+          <Link to="/services/" state={{ toID: 'Investment_Solutions' }}>
+            <p className="footer__right__content__list__item__sub">
+              Investment Solutions
+            </p>
+          </Link>
         </li>
         <li className="footer__right__content__list__item">
           <Link to="/about/">
